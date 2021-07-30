044e2d3ef6253981876df6e107f5f7cec25546b9
Roh <ronaldogoudinho815@gmail.com>
2021-07-30 17:35:10 -0300

    Criado o component para a tela home e para o item destaque

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FeaturedItemComponent } from './featured-item/featured-item.component';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FeaturedItemComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,
+     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
